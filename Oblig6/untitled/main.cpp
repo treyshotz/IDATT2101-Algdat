80fa86dbc00db0e5e78ee73be1080e2859642091
Mads Lundegaard <mads.lundegaard@live.no>
2020-10-09 13:30:50 +0200

    Added comments

--- Oblig6/untitled/main.cpp
+++ Oblig6/untitled/main.cpp
@@ -18,15 +18,30 @@ public:
     explicit Graph(int nodes, int edges) {
         this->nodes = nodes;
         this->edges = edges;
-        adjList2 = new vector<intPair>(edges);
+        //Cant seem to find out why i can't use this list...
+        //adjList2 = new vector<intPair>(nodes+1);
     }
 
+    /**
+     * Adds an edge to the input list
+     * @param adjacency_list to add to
+     * @param node1 place to add
+     * @param node2 neighbour
+     * @param weight
+     */
     void addEdge(vector<pair<int, int> > adjacency_list[], int node1, int node2, int weight) {
+        //this->adjList2[node1].emplace_back(node2, weight);
         adjacency_list[node1].emplace_back(node2, weight);
 
+        //If it where undirected graph
         //adjacency_list[node2].emplace_back(node1, weight);
     }
 
+    /**
+     * Finds the shortest path from every node to a start point
+     * @param adjacency_list list containing int pairs
+     * @param start node
+     */
     void shortestPath(vector<pair<int, int>> adjacency_list[], int start) {
 
         priority_queue<intPair, vector<intPair>, greater<intPair> > heap;
@@ -54,7 +69,7 @@ public:
         cout << "Node\t \t Distance" << endl;
         for (int i = 0; i < nodes; i++) {
             if (distance[i] == INT_MAX) {
-                cout << i << "\t \t \t " <<"Nåes ikke" << endl;
+                cout << i << "\t \t \t " <<"Cannot be reached" << endl;
             } else {
                 cout << i << "\t \t \t " << distance[i] << endl;
             }
@@ -65,26 +80,27 @@ public:
 
 int main() {
     //Sets starting point on the graph
-    int start = 0;
+    int start = 1;
 
     //Sets up graph
     int nodes;
     int edges;
     string str;
-    ifstream file("/Users/madslun/Documents/Programmering/AlgDat/Oblig6/untitled/vgSkandinavia.txt");
+    ifstream file("/Users/madslun/Documents/Programmering/AlgDat/Oblig6/untitled/vg1.txt");
     getline(file, str);
     istringstream iss(str);
     iss >> nodes;
     iss >> edges;
 
-    //Creating the list which will contain all the edges
+    //Creating
     Graph graph(nodes, edges);
+    //Creating the list which will contain all the edges
     vector<intPair> adjList[nodes+1];
-
     int node1;
     int node2;
     int weight;
     for (int i = 0; i < edges; i++) {
+
         getline(file, str);
         istringstream iss2(str);
         iss2 >> node1;
